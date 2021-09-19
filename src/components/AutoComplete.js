5cdccb007367b8ee7cf25b6d20b390bcc7ec0dbf
A Oladipupo-Usoro <aolausoro@gmail.com>
2021-09-19 11:59:04 +0100

    first commit

---
+++ src/components/AutoComplete.js
@@ -0,0 +1,15 @@
+function AutoComplete({ isVisible, suggestions, handleSuggestionClick }) {
+  return (
+    <div className={`${isVisible ? 'show suggestion-box' : 'suggestion-box'}`}>
+      <ul>
+        {suggestions.map((country, index) => (
+          <li key={index} onClick={() => handleSuggestionClick(country)}>
+            {country}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default AutoComplete;
